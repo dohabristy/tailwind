047ae318cb831a5df2e986aaa92f1842617e676a
dohabristy <labornodb@gmail.com>
2023-10-06 04:05:21 +0600

    tailwind

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            clifford: '#da373d',
+            'navbg': '#0D0D0D',
+            'white': '#fff',
+            'btn': '#FFD2A4',
+          },
+        maxWidth: {
+            'container': '1280px',
+          },
+        fontFamily: {
+            'roboto': 'Roboto Condensed',
+            
+          },
+      }
+    }
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            clifford: '#da373d',
+            'navbg': '#0D0D0D',
+            'white': '#fff',
+          },
+        maxWidth: {
+            'container': '1280px',
+          },
+        backgroundImage: {
+            'banner': "url('')",
+          },
+      }
+    }
+  }
